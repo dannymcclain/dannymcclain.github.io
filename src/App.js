58daf5c355193d7128d1a521bff012d25ebad245
Danny <dannyxmcclain@gmail.com>
2018-09-20 14:37:58 -0500

    updated emoji and favicon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Code, MessageCircle, Heart, Mail } from 'react-feather';
 import Link from './Components/Link';
+import Emoji from './Components/Emoji';
 import './App.css';
 
 const App = () => (
   <div className="container">
+    <Emoji symbol="👨‍💻" />
     <h1>Danny McClain</h1>
     <p>
       I’m a digital product designer crafting apps and interfaces. Currently
