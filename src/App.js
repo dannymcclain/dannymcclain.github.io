8eefeb2236a051da669f3a3d220be7cf8aa3b1d8
Danny <dannyxmcclain@gmail.com>
2018-09-20 13:42:11 -0500

    test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 const App = () => (
   <div className="container">
-    <h1>Danny McClain</h1>
+    <h2>Danny McClain</h1>
     <p>
       I’m a digital product designer crafting apps and interfaces. Currently
       having lots of fun learning React.
