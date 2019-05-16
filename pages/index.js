45225233ae9b5e87bf7b6b7313f63cf5262755a8
Danny <dannyxmcclain@gmail.com>
2019-05-16 12:47:06 -0500

    filling out styles and content

--- pages/index.js
+++ pages/index.js
@@ -30,23 +30,93 @@ export default () => (
     </Head>
     <header>
       <h1>Danny McClain</h1>
-      <p />
+      <p>
+        Digital product designer who likes to build stuff too. Always learning
+        because learning is fun!
+      </p>
     </header>
     <section className="about">
       <h2>About</h2>
-      <p />
-      <p />
+      <p>
+        I design apps and interfaces that work well, look nice, and feel good.
+      </p>
+      <p>
+        I like systems, icons, typography, strategy, designing based on
+        research, iterating constantly, and shipping.
+      </p>
     </section>
     <section className="projects">
       <h2>Misc. Projects</h2>
+      <ul>
+        <li>
+          <a href="" target="_blank">
+            Fit to Parent
+          </a>
+        </li>
+        <li>
+          <a href="" target="_blank">
+            Swap Width &amp; Height
+          </a>
+        </li>
+        <li>
+          <a href="" target="_blank">
+            DoodleFrame UI Kit
+          </a>
+        </li>
+        <li>
+          <a href="" target="_blank">
+            Ratio
+          </a>
+        </li>
+        <li>
+          <a href="" target="_blank">
+            Grid Calc
+          </a>
+        </li>
+      </ul>
+      <p>
+        View more of my work on{' '}
+        <a href="" target="_blank">
+          Dribbble
+        </a>
+        .
+      </p>
     </section>
     <section className="currently">
       <h2>Currently</h2>
-      <p />
+      <p>
+        I’m currently designing digital experiences that help people get a smile
+        they’re proud of at{' '}
+        <a href="" target="_blank">
+          SmileDirectClub
+        </a>
+        .
+      </p>
     </section>
     <section className="links">
       <h2>Links</h2>
-      <ul />
+      <ul>
+        <li>
+          <a href="" target="_blank">
+            Dribbble
+          </a>
+        </li>
+        <li>
+          <a href="" target="_blank">
+            GitHub
+          </a>
+        </li>
+        <li>
+          <a href="" target="_blank">
+            Twitter
+          </a>
+        </li>
+        <li>
+          <a href="" target="_blank">
+            Email
+          </a>
+        </li>
+      </ul>
     </section>
   </div>
 );
